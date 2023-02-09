a585af8d197e542e303be5ca9524270276898b0e
ananthula manoj <hemanth.hemanth180@gmail.com>
2023-02-09 20:09:46 +1100

    first commit

---
+++ helloworld.py
@@ -0,0 +1,13 @@
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+
+
+class MyApp(App):
+
+	def build(self):
+		return Label(text="Hello World!", font_size=67)
+
+
+if __name__ == "__main__":
+	MyApp().run()
